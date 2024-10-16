4469950f93e05dbe1a0d42e40a6f3d990e45ec2c
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-10-16 17:03:41 +0300

    Update Circuit4.v

--- Tests/Circuit4/Circuit4.v
+++ Tests/Circuit4/Circuit4.v
@@ -1,1 +1,25 @@
-module DEC4to16(input A, input B, input C, input D, output out);    (~a&c&~d)+ (a&~c&~d) + (b&c&~d);endmodule
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2024 06:34:24 PM
+// Design Name: 
+// Module Name: Circuit4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module DEC4to16(input A, input B, input C, input D, output out);
+    assign out = (~a&c&~d)+ (a&~c&~d) + (b&c&~d);
+endmodule
+
