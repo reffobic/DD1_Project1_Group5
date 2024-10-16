bc339b5b78dcf1b8d42bdd198f1c8977c4658aec
Hana Shalaby <shalaby04@aucegypt.edu>
2024-10-16 16:41:22 +0300

    Create Circuit5.v

---
+++ Tests/Circuit5/Circuit5.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2024 06:34:24 PM
+// Design Name: 
+// Module Name: Circuit1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Circuit5(input A, B, C, output out);
+
+  assign out = ~(A&B) | (B&C);
+
+endmodule
