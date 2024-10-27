1fa4f3f9a563204e3f41e1a39929d191aeb7a8fc
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-10-27 12:36:35 +0300

    Update Circuit5.v

--- Tests/Circuit5/Circuit5.v
+++ Tests/Circuit5/Circuit5.v
@@ -9,6 +9,6 @@ module circuit_5(a, b, c, out);
     wire ab_nand;
 
     nand #(5) g0 (ab_nand, a, b);
-    or #(5) g1 (out, ab_nand, w2);
     and #(5) g2 (w2, b, c);
+    or #(5) g1 (out, ab_nand, w2);
 endmodule
