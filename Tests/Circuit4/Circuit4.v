32c5ab2ea222b06e2f0e3b4d36799098ebbdf28b
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-10-27 12:35:12 +0300

    Update Circuit4.v

--- Tests/Circuit4/Circuit4.v
+++ Tests/Circuit4/Circuit4.v
@@ -1,25 +1,22 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/14/2024 06:34:24 PM
-// Design Name: 
-// Module Name: Circuit4
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module dec4to16(a, b, c, d, out);
+    input a;
+    input b;
+    input c;
+    input d;
+    output out;
 
-module DEC4to16(input A, input B, input C, input D, output out);
-    assign out = (~a&c&~d)+ (a&~c&~d) + (b&c&~d);
+    wire w1;
+    wire w2;
+    wire w3;
+    wire a_not;
+    wire c_not;
+    wire d_not;
+
+    not #(2) g0 (a_not, a);
+    not #(2) g1 (c_not, c);
+    not #(2) g2 (d_not, d);
+    and #(5) g3 (w1, a_not, c, d_not);
+    and #(5) g4 (w2, a, c_not, d_not);
+    and #(5) g5 (w3, b, c, d_not);
+    or #(5) g6 (out, w1, w2, w3);
 endmodule
-
