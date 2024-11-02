7ccc20da0db64b7d784e86e767d54f7c9c0e8af7
maz0uz <124688655+maz0uz@users.noreply.github.com>
2024-11-02 17:46:50 +0200

    Update LogicGate.hpp

--- Updated Code/LogicGate.hpp
+++ Updated Code/LogicGate.hpp
@@ -13,31 +13,30 @@
 #include <vector>
 using namespace std;
 
-struct ioVar {
-    string name;
-    bool val;
-    int index;
-    vector<int> affectedGateInd;
-    string type;
+struct ioVar { // struct for inputs, outputs, or wires
+    string name; // stores the name
+    bool val; // stores the value
+    int index; // stores the index in the vector to avoid having to search for it
+    vector<int> affectedGateInd; // stores the index of any gates for which this variable is an input
+    string type; // stores the type of variable (wire, input, output)
 };
 
-class logicGate
+class logicGate // Class for the gates
 {
 private:
-    string name;
-    int delay;
-    vector<ioVar> inputs;
-    bool output;
-    int outputInd;
-    string outType;
-    string gate_type;
-    void updateInputs(vector<ioVar> inputVars, vector<ioVar> wireVars);
+    string name; // stores the name of the gate
+    int delay; // stores the propagation delay of this gate
+    vector<ioVar> inputs; // stores the input variables of this gate
+    bool output; // stores the output value of this gate
+    int outputInd; // stores the index of the output in its relevant vector
+    string outType; // stores the type of output (wire or output)
+    string gate_type; // stores the gate type (and, or, etc.)
+    void updateInputs(vector<ioVar> inputVars, vector<ioVar> wireVars); // function used to update the values of the vectors to reflect those in the main
 
 public:
-    logicGate();
-    logicGate(string n, int d, vector<int> inp, int outp, string type, string outType, vector<string> inTypes);
-    int getDelay();
-    int getOut(string& type);
-    bool evaluate(bool &out, vector<ioVar> inputVec, vector<ioVar>wireVec);
+    logicGate(string n, int d, vector<int> inp, int outp, string type, string outType, vector<string> inTypes); // Constructor to initialize attributes
+    int getDelay(); // getter returns the propagation delay of the gate
+    int getOut(string& type); // getter to return the index of the output and its type by reference
+    bool evaluate(bool &out, vector<ioVar> inputVec, vector<ioVar>wireVec); // function to evaluate the gate
 };
 #endif /* LogicGate_h */
