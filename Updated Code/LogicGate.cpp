40b90191f042d5e2a4d25e5cf57ecbb4619627c7
Mohamed El Refai <133569987+reffobic@users.noreply.github.com>
2024-11-02 23:07:20 +0200

    Update LogicGate.cpp

--- Updated Code/LogicGate.cpp
+++ Updated Code/LogicGate.cpp
@@ -1,5 +1,8 @@
 #include "LogicGate.hpp"
 
+logicGate::logicGate() {
+    name = "";
+}
 
 // Constructor sets the values for most of the gate's attributes
 logicGate::logicGate(string n, int d, vector<int> inp, int outp, string type, string outputType, vector<string> inTypes) {
@@ -18,10 +21,13 @@ logicGate::logicGate(string n, int d, vector<int> inp, int outp, string type, st
     }
 }
 
-bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVec) { // evaluate function to check the output of the gate
-    updateInputs(inputVec, wireVec); // updates the inputs to reflect any change of values in the main
+// evaluate function to check the output of the gate
+bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVec) {
+    // updates the inputs to reflect any change of values in the main
+    updateInputs(inputVec, wireVec);
 
-    if (gate_type == "and") { // If the gate is an and gate, it checks if any of the inputs are false. If 1 is false, it stores the output as false
+    // If the gate is an and gate, it checks if any of the inputs are false. If 1 is false, it stores the output as false
+    if (gate_type == "and") {
         out = true;
         for (int i = 0; i < inputs.size(); i++) {
             if (!inputs[i].val) {
@@ -29,7 +35,9 @@ bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVe
                 break;
             }
         }
-    } else if (gate_type == "or") { // If the gate is an or gate, it checks if any of the inputs are true. If so, it stores the output as true
+    }
+    // If the gate is an or gate, it checks if any of the inputs are true. If so, it stores the output as true
+    else if (gate_type == "or") {
         out = false;
         for (int i = 0; i < inputs.size(); i++) {
             if (inputs[i].val) {
@@ -37,18 +45,25 @@ bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVe
                 break;
             }
         }
-    } else if (gate_type == "not") { // if the gate is a not gate, it takes the first input (since there can only be 1 input it is assumed to be the first) and returns its inversion
+    }
+    // if the gate is a not gate, it takes the first input (since there can only be 1 input it is assumed to be the first) and returns its inversion
+    else if (gate_type == "not") {
         out = !inputs[0].val;
-    } else if (gate_type == "nand") { // If the gate is a nand gate, it checks for at least 1 0, if it is found it stores the output as true.
+    
+    }
+    // If the gate is a nand gate, it checks for at least 1 0, if it is found it stores the output as true.
+    else if (gate_type == "nand") {
         out = true;
         for (int i = 0; i < inputs.size(); i++) {
             if (!inputs[i].val) {
                 out = true;
-                break;
+                return out;
             }
         }
         out = false;
-    } else if (gate_type == "nor") { // If the gate is a nand gate, it checks for at least 1 1, if so it stores the output as false
+    }
+    // If the gate is a nor gate, it checks for at least 1 1, if so it stores the output as false
+    else if (gate_type == "nor") {
         out = true;
         for (int i = 0; i < inputs.size(); i++) {
             if (inputs[i].val) {
@@ -56,7 +71,9 @@ bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVe
                 break;
             }
         }
-    } else if (gate_type == "xor") { // If the gate is an xor gate, it checks if there is an odd number of 1s. If so, it stores the output as true
+    }
+    // If the gate is an xor gate, it checks if there is an odd number of 1s. If so, it stores the output as true
+    else if (gate_type == "xor") {
         int trueCount = 0;
         for (int i = 0; i < inputs.size(); i++) {
             if (inputs[i].val) {
@@ -64,7 +81,9 @@ bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVe
             }
         }
         out = (trueCount % 2 == 1);
-    } else if (gate_type == "xnor") { // If the gate is an xnor gate, it checks if there is an even number of 1s. If so, it stores the output as true.
+    }
+    // If the gate is an xnor gate, it checks if there is an even number of 1s. If so, it stores the output as true.
+    else if (gate_type == "xnor") {
         int trueCount = 0;
         for (int i = 0; i < inputs.size(); i++) {
             if (inputs[i].val) {
@@ -76,19 +95,22 @@ bool logicGate::evaluate(bool& out, vector<ioVar> inputVec, vector<ioVar> wireVe
         return false;
     }
 
-    return out;
+    return true;
 }
 
-int logicGate::getDelay() { // returns the delay
+// returns the delay
+int logicGate::getDelay() {
     return delay;
 }
 
-int logicGate::getOut(string& type) { // stores the type in the parameter and returns the index of the gate's output variable
+// stores the type in the parameter and returns the index of the gate's output variable
+int logicGate::getOut(string& type) {
     type = outType;
     return outputInd;
 }
 
-void logicGate::updateInputs(vector<ioVar> inputVars, vector<ioVar> wires) { // updates the inputs of the gate to reflect any change in values in the main.
+// updates the inputs of the gate to reflect any change in values in the main.
+void logicGate::updateInputs(vector<ioVar> inputVars, vector<ioVar> wires) {
     for (int i = 0; i < inputs.size(); i++) {
         if (inputs[i].type == "input") {
             inputs[i].val = inputVars[inputs[i].index].val;
